9d84501968e72492630e8e25568217d2c022f9cc
EO-TKowalczyk <53469412+EO-TKowalczyk@users.noreply.github.com>
2020-03-29 14:23:58 +0100

    My own weird solution
    This takes the array, puts it in a string and back into an array, then formats it appropriately before printing it to screen

--- src/App.js
+++ src/App.js
@@ -2,45 +2,67 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Post from './Post';
 
-// import {Row,Col} from "react-flexbox-grid";
-  class App extends Component {
-    state = { posts:[] };
-      
-    componentDidMount () {
-      axios.get('https://api.exchangeratesapi.io/latest')
-      .then(response => {
-        this.setState({posts: response.data}) ;
-        console.log(response.data);
-        console.log(response.data.rates);
-        console.log(response.data.rates.PHP);
-        console.log(response.data.base);
-        console.log(response.data.date);
-      });
-    }
+class App extends React.Component {
+    state = {
+        isLoading: true,
+		rates: []
+	}
+	
+	render() {
+		const { isLoading, rates, error } = this.state;
+		return (
+		<React.Fragment>
+			<h1>Exchange Rates</h1>
+			
+			{error ? <p>{error.message}</p> : null}
+			
+			{!isLoading ? (
+				rates.map(rate => {
+					rate = JSON.stringify(rate).replace(/[&\/\\{}"]/g,'').split(":");
+					console.log("given rate: ", rate);
+					const name = rate[0];
+					const price = rate[1];
+					console.log("given rate: ", rate);
+					return (
+						<div key={name}>
+							<p>Name: {name}</p>
+							<p>Price: £{(Math.round(price*100)/100).toFixed(2)}</p>
+							<hr />
+						</div>
+					);
+				})
+			// If there is a delay in data, let's let the user know it's loading
+			) : (
+			<h3>Loading...</h3>
+			)}
+		</React.Fragment>
+		);
+	}
 
-    render() {
-     
-      if (!this.state.posts.length){
-        return <div>didn't post anything</div>
-      }
+	fetchUsers() {
+		// Where we're fetching data from
+		fetch('https://api.exchangeratesapi.io/latest')
 
+		// We get the API response and receive data in JSON format...
+		.then((response) => {
+			return response.json();
+		})
 
-         return(
-         <div>
-           {this.state.posts.map(post =>(
-             <div>
-                <div>{post.rates.rates}</div>
+		.then((data) => {
+			console.log("data: ", data);
+			console.log("rates: ", data.rates);
+			console.log("formatted rates: ", JSON.stringify(data.rates).split(','));
+			this.setState({ 
+				isLoading: false,
+				rates: JSON.stringify(data.rates).split(',')
+			})
+		});
+	}
 
+	componentDidMount() {
+		this.fetchUsers();
+	}
 
+}
 
-             </div>
-
-           ))}
-         </div>
-        ) 
-     
-    }
-  }
 export default App;
-
-// rates={posts.rates} 
